156c57a64d86a4c4c747a6c7f865fc476ae50abe
Mathews, Jaish <jmathews4@dxc.com>
2021-02-24 15:53:37 +0530

    Added additional unit test cases for API layer

--- AdventureWorks.API.Tests/Controllers/ProductControllerTests.cs
+++ AdventureWorks.API.Tests/Controllers/ProductControllerTests.cs
@@ -1,38 +1,72 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using AdventureWorks.API.Controllers;
-using System;
+﻿using AdventureWorks.API.Tests;
+using AdventureWorks.Business.Interface;
+using AdventureWorks.Business.Model;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace AdventureWorks.API.Controllers.Tests
 {
 	[TestClass()]
-	public class ProductControllerTests
+	public class ProductControllerTests : TestBase
 	{
-		[TestMethod()]
-		public void ProductControllerTest()
+		Mock<IProductsBusiness> _productBusinessMock;
+		Mock<ILogger<ProductController>> _loggerMock;
+		Mock<IMapper> _mapperMock;
+
+		[TestInitialize()]
+		public new void Initialize()
 		{
-			Assert.Fail();
+			_productBusinessMock = new Mock<IProductsBusiness>();
+			_loggerMock = new Mock<ILogger<ProductController>>();
+			_mapperMock = new Mock<IMapper>();
 		}
 
 		[TestMethod()]
-		public void GetProductsTest()
+		public async Task GetProductsTest()
 		{
-			Assert.Fail();
+			ProductController productController = new ProductController(_loggerMock.Object, _productBusinessMock.Object, _mapperMock.Object);
+			_productBusinessMock.Setup(p => p.GetProducts()).ReturnsAsync(ProductsList);
+			List<Product> productList = await productController.GetProducts() as List<Product>;
+			Assert.AreEqual(2, productList.Count());
+			CollectionAssert.AllItemsAreInstancesOfType(productList, typeof(Product));
 		}
 
 		[TestMethod()]
-		public void GetProductsV2Test()
+		public async Task GetProductsV2Test()
 		{
-			Assert.Fail();
+			ProductController productController = new ProductController(_loggerMock.Object, _productBusinessMock.Object, _mapperMock.Object);
+			_productBusinessMock.Setup(p => p.GetProducts()).ReturnsAsync(ProductsList);
+			ObjectResult objectResult = await productController.GetProductsV2() as ObjectResult;
+			var productListReturned = objectResult.Value as List<Product>;
+			Assert.AreEqual(2, productListReturned.Count());
+			Assert.AreEqual(200, objectResult.StatusCode);
+			CollectionAssert.AllItemsAreInstancesOfType(productListReturned, typeof(Product));
 		}
 
 		[TestMethod()]
-		public void SaveProductsTest()
+		public async Task SaveProductsCreatedStatusTest()
 		{
-			Assert.Fail();
+			var product = new Product();
+			ProductController productController = new ProductController(_loggerMock.Object, _productBusinessMock.Object, _mapperMock.Object);
+			_productBusinessMock.Setup(p => p.SaveProducts(product)).ReturnsAsync(1);
+			StatusCodeResult statusCodeResult = await productController.SaveProducts(product) as StatusCodeResult;
+			Assert.AreEqual(201, statusCodeResult.StatusCode);
+		}
+
+		[TestMethod()]
+		public async Task SaveProductsInternalServerErrorStatusTest()
+		{
+			var product = new Product();
+			ProductController productController = new ProductController(_loggerMock.Object, _productBusinessMock.Object, _mapperMock.Object);
+			_productBusinessMock.Setup(p => p.SaveProducts(product)).ReturnsAsync(0);
+			StatusCodeResult statusCodeResult = await productController.SaveProducts(product) as StatusCodeResult;
+			Assert.AreEqual(500, statusCodeResult.StatusCode);
 		}
 	}
 }
---
+++ AdventureWorks.API.Tests/TestBase.cs
@@ -0,0 +1,56 @@
+﻿using AdventureWorks.Business.Model;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AdventureWorks.API.Tests
+{
+	[TestClass()]
+	public class TestBase
+	{
+		protected static List<Product> ProductsList { get; private set; }
+
+		[AssemblyInitialize()]
+		public static void AssemblyInit(TestContext context)
+		{
+			ProductsList = new List<Product>();
+			ProductsList.Add(new Product { ProductId = 100, Name = "Product1", ProductNumber = "1000", StandardCost = 1000, SellStartDate = DateTime.Today, Color = null, Size = null });
+			ProductsList.Add(new Product { ProductId = 200, Name = "Product2", ProductNumber = "2000", StandardCost = 1000, SellStartDate = DateTime.Today, Color = null, Size = null });
+		}
+
+		[ExcludeFromCodeCoverage]
+		[ClassInitialize()]
+		public static void ClassInit(TestContext context)
+		{
+
+		}
+
+		[TestInitialize()]
+		public void Initialize()
+		{
+
+		}
+
+		[TestCleanup()]
+		public void Cleanup()
+		{
+
+		}
+		[ExcludeFromCodeCoverage]
+		[ClassCleanup()]
+		public static void ClassCleanup()
+		{
+
+		}
+
+		[AssemblyCleanup()]
+		public static void AssemblyCleanup()
+		{
+
+		}
+
+	}
+}
